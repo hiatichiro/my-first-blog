bc06288f96596ccc7d92e0f23b2de1d07959ee19
hiatichiro <sroemer97@gmail.com>
2020-12-06 03:51:40 +0000

    9

--- mysite/settings.py
+++ mysite/settings.py
@@ -120,3 +120,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
