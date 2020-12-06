3674052c750b0cb002e36f99c2f3d0f11b39b79b
hiatichiro <sroemer97@gmail.com>
2020-12-05 20:16:06 -0600

    8

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,6 @@ SECRET_KEY = '!js6lbhoy3wniabct*oxh4h_y39jr4#k1)+v6506vzi!u-u!!+'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
 
 
 # Application definition
